3e401640947ea77a658d67f0529a3c757f8e82e6
shakra172 <boyshakra172@gmail.com>
2026-06-27 17:08:14 +0300

    exercise4 and images exercise4

---
+++ exercise4/main.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+    return a+b;
+}
+let result = add(5,10);
+console.log(result);
+let div = add(10,70);
+console.log(div / 2);
+let mul = add(5,10);
+console.log(mul * 2);
+let subs = add(20,10);
+console.log(subs - 15);
+let all = add(100,100);
+console.log(all *2  / 4 -50 );
+
+let greet = function(pragraph) {
+    console.log("hello somalia people " + pragraph)
+}
+greet('my name is Abdishakour today i want to telling you spomething specially i would say happy independence day');
+
+const number = function(adding) {
+    console.log(10+10 + adding);
+
+}
+number(20 / 2);
+number(100 / 4);
+number(60 / 2);
+number(10 / 2);
+number(2000 / 2);
