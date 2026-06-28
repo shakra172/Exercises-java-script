bf5cfe9b8f974246a6865915a17d2ca994675193
shakra172 <boyshakra172@gmail.com>
2026-06-28 12:24:56 +0300

    exercise5 and images exercise5

---
+++ exercise5/main.js
@@ -0,0 +1,27 @@
+let greet = (name) => {
+    console.log ("hello " + name);
+
+}
+greet("shakra");
+greet("sumaya");
+
+let how =(sumaya) => {
+    console.log("how about you " +sumaya);
+}
+how("I'm goood");
+
+var good = (shakra) => {
+    console.log("and you " + shakra);
+}
+good("also I'm goood ");
+
+
+const number =(adding) => {
+    console.log(10+10 + adding);
+
+}
+number(20 / 2);
+number(100 / 4);
+number(60 / 2);
+number(10 / 2);
+number(2000 / 2);
