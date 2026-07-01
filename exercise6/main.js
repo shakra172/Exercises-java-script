9d69d1d6dc41ac23bc849890bdeacdedb2d2524c
shakra172 <boyshakra172@gmail.com>
2026-07-01 10:14:46 +0300

    exercise6 and images exercise6

---
+++ exercise6/main.js
@@ -0,0 +1,14 @@
+let color = ["red", "green", "blue", "yellow"];
+console.log(color);
+
+console.log(color[3]);
+
+console.log(color[0]);
+
+console.log(color.length);
+
+let list = [1, 2, 3, 4, 5];
+console.log(list);
+
+console.log(list[4]);
+console.log(list.length);
