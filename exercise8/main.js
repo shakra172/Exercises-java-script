803c0e8a68643bc9e68fc4766ab4378f84204b05
shakra172 <boyshakra172@gmail.com>
2026-07-01 10:37:31 +0300

    exercise8 and images exercise8

---
+++ exercise8/main.js
@@ -0,0 +1,21 @@
+let car = {
+    make: "Toyoto",
+    model: "luxury",
+    year: 2020,
+
+}
+
+console.log(car.make);
+console.log(car.model);
+console.log(car["year"]);
+
+let person = {
+    name : "shakra",
+    id : "C1230720",
+    age : 20,
+    year : 2023,
+    faculty : "IT",
+    city : "afgooye",
+}
+
+console.log(person);
