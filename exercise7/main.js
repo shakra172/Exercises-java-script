20acc0a0fef5a71f7018aa4eadfc8fe1a46ffe92
shakra172 <boyshakra172@gmail.com>
2026-07-01 10:26:30 +0300

    exercise7 and images exercise7

---
+++ exercise7/main.js
@@ -0,0 +1,26 @@
+let color = ["red", "green", "blue", "yellow"];
+console.log(color);
+
+console.log(color[3]);
+
+console.log(color[0]);
+
+console.log(color.length);
+
+let list = [1, 2, 3, 4, 5];
+console.log(list);
+
+console.log(list[4]);
+console.log(list.length);
+
+after = color.push("white");
+console.log(color);
+
+after = color.shift();
+console.log(color);
+
+after = list.push(6);
+console.log(list);
+
+after = list.shift();
+console.log(list);
