a664c05fe15fa78375c8c50cbb7045238a865e9c
shakra172 <boyshakra172@gmail.com>
2026-07-04 18:17:28 +0300

    exercise12 and images exercise12

---
+++ exercise12/shakra.js
@@ -0,0 +1,9 @@
+const fruits = ["apple", "banana", "orange", "lemons","mangoes", "strawberry", "watermelon"];
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i])
+// }
+
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
