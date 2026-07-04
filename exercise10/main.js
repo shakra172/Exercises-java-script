4960333a6af3b816d19734caada001f9010e755e
shakra172 <boyshakra172@gmail.com>
2026-07-04 14:41:58 +0300

    exercise10 & and images exercise10

---
+++ exercise10/main.js
@@ -0,0 +1,18 @@
+let library = ["title", "author", "year"];
+
+let librarys = [
+    {
+        title: "The power of money",
+        author: "Updishakour",
+        year: 2025,
+    },
+    {
+        title: "book of death",
+        author: "iqro",
+        year: 2021,
+    },
+    
+];
+
+console.log(librarys[0].title);
+console.log(librarys[1].author);
